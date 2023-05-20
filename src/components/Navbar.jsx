a0b8f1840d7879d6d25d371026711a9b79567287
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-05-19 23:35:16 -0600

    Se agrego navbar nuevo al proyecto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,86 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import LogoutIcon from '@mui/icons-material/Logout';
-
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faShoppingCart, faBell } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
-
-    const cerrarSesion = () => {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-    };
-
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <Link className="navbar-brand mt-2 mt-lg-0" to="/">
+            <img
+              src="https://cdn2.steamgriddb.com/file/sgdb-cdn/icon/851b10e5b105815806efcf78b53588f4/32/256x256.png"
+              height="30"
+              alt="Hotel Logo"
+              loading="lazy"
+            />
+          </Link>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/dashboard">
+                Hoteles
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/team">
+                Eventos
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/projects">
+                Reservacion
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="d-flex align-items-center">
+          <Link className="text-reset me-3" to="#">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </Link>
+          <div className="dropdown">
+            <Link
+              className="text-reset me-3 dropdown-toggle hidden-arrow"
+              to="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
             >
-                Asociaciones
+              <FontAwesomeIcon icon={faBell} />
+              <span className="badge rounded-pill badge-notification bg-danger">1</span>
             </Link>
-
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-
-                    <NavLink 
-                        className= {({isActive}) =>`nav-item nav-link ${isActive ? 'active':''}`}
-                        to="/hotel"
-                    >
-                        Hoteles
-                    </NavLink>
-
-                    <NavLink 
-                        className= {({isActive}) =>`nav-item nav-link ${isActive ? 'active':''}`}
-                        to="/usuario"
-                    >
-                        Usuario
-                    </NavLink>
-
-                    <NavLink 
-                        className= {({isActive}) =>`nav-item nav-link ${isActive ? 'active':''}`}
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
-                </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-primary'>
-                        Andersson
-                    </span>
-                    <Link
-                    className='nav-link'
-                    to="/login"
-                    onClick={() => cerrarSesion()}
-                    >
-                    <LogoutIcon fontSize="medium"></LogoutIcon>
-                    </Link>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+              <li>
+                <NavLink className="dropdown-item" to="/profile">
+                  My profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="dropdown-item" to="/settings">
+                  Settings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="dropdown-item" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
