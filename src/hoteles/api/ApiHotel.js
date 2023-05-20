37d33e6a772fbf2e9554c02ac51fdced1725f9dc
anderssonU17 <aurrea-2019284@kinal.edu.gt>
2023-05-19 21:58:45 -0600

    Se agrego verificacion de Array

--- src/hoteles/api/ApiHotel.js
+++ src/hoteles/api/ApiHotel.js
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 const URL = 'http://localhost:3005/api/';
 
-export const listHotels = async() => {
-  try{
-    const {data: {Hotel}} = await axios.get(`${URL}read-hotels`);
-    console.log(Hotel);
-    return Hotel;
-  }catch(error){
-    return error;
+export const listHotels = async () => {
+  try {
+    const response = await axios.get(`${URL}read-hotels`);
+    console.log(response);
+    if (response.data && Array.isArray(response.data['Hoteles encontrados'])) {
+      return response.data['Hoteles encontrados'];
+    } else {
+      console.error("La respuesta no contiene una matriz de hoteles:", response.data);
+      return [];
+    }
+  } catch (error) {
+    console.error("Error al obtener los hoteles:", error);
+    return [];
   }
-}
+};
--- src/hoteles/pages/HotelesPage.jsx
+++ src/hoteles/pages/HotelesPage.jsx
@@ -1,57 +1,69 @@
 import React, { useEffect, useState } from "react";
-import {listHotels} from '../api/ApiHotel.js';
+import { listHotels } from '../api/ApiHotel.js';
 
 export const HotelesPage = () => {
   const [hotels, setHotels] = useState([]);
 
-  const reload = async() => {
-    const result = await listHotels();
-    setHotels(result);
-  };
-
   useEffect(() => {
-    reload();
-    console.log(hotels);
-  }, [])
-  
+    const fetchHotels = async () => {
+      try {
+        const result = await listHotels();
+        if (Array.isArray(result)) {
+          setHotels(result);
+        } else {
+          console.error("Los datos devueltos no son un array:", result);
+        }
+      } catch (error) {
+        console.error("Error fetching hotels:", error);
+      }
+    };
+
+    fetchHotels();
+  }, []);
 
   return (
-    // Codigo a modificar y pasar a cards 
     <>
-        <h1 className="text-decoration-underline">CRUD DE HOTELES</h1>
-        <div>
-            <table className="table table-bordered">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre de Hotel</th>
-                    <th>Descripcion</th>
-                    <th>Direccion</th>
-                    <th>Opciones</th>
+      <h1 className="text-decoration-underline">CRUD DE HOTELES</h1>
+      <div>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre de Hotel</th>
+              <th>Descripcion</th>
+              <th>Direccion</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(hotels) && hotels.length === 0 ? (
+              <tr>
+                <td colSpan={5}>No se han encontrado hoteles registrados.</td>
+              </tr>
+            ) : (
+              hotels.map(hotelActual => (
+                <tr key={hotelActual._id}>
+                  <td>{hotelActual._id}</td>
+                  <td>{hotelActual.name}</td>
+                  <td>{hotelActual.description}</td>
+                  <td>{hotelActual.address}</td>
+                  <td>
+                    <button className='btn btn-danger margen-button'>
+                      Eliminar
+                    </button>
+                    <button className='btn btn-warning margen-button'>
+                      Editar
+                    </button>
+                    <button className='btn btn-success margen-button'>
+                      Ver
+                    </button>
+                  </td>
                 </tr>
-                </thead>
-                <tbody>
-                  // Undefined maps 
-                    {hotels.map(hotelActual=>{
-                        return(
-                            <tr key={hotelActual._id}>
-                        <td>{hotelActual._id}</td>
-                        <td>{hotelActual.name}</td>
-                        <td>{hotelActual.description}</td>
-                        <td>{hotelActual.address}</td>
-                        <td>
-                            <button className='btn btn-danger margen-button'>
-                                Eliminar
-                                </button>
-                            <button className='btn btn-warning margen-button'>Editar</button>
-                            <button className='btn btn-success margen-button'>Ver</button>
-                        </td>
-                    </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </div>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
-}
+  );
+};
